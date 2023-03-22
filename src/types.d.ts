6c170e20c3191aa27ae4028828fde61fc3f94961
francescoalterio <francescoalteriog@gmail.com>
2023-03-22 10:01:46 -0400

    second commit

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+interface Project {
+  id: number;
+  name: string;
+  fileName: string;
+  alt: string;
+  description: string;
+  preview: string[];
+  repository: string[];
+  technologies: string[];
+  isFavorite: boolean;
+}
