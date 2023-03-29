c0de50b320a9a0fde4f67f0ce52719ed4fb7ede9
francescoalterio <francescoalteriog@gmail.com>
2023-03-29 13:11:48 -0400

    new PresentationCard, with react Component NumberOfRepos

---
+++ src/components/NumberOfRepos.tsx
@@ -0,0 +1,22 @@
+import { useState, useEffect } from "react";
+
+export function NumberOfRepos() {
+  const [reposLength, setReposLength] = useState(0);
+
+  useEffect(() => {
+    async function getReposLength() {
+      const githubURL = `https://api.github.com/user/repos?per_page=1000`;
+      const resGithub = await fetch(githubURL, {
+        headers: {
+          Authorization: `token ${import.meta.env.PUBLIC_GITHUB_TOKEN}`,
+        },
+      });
+      const repos = await resGithub.json();
+      console.log(repos);
+      setReposLength(repos.length);
+    }
+    getReposLength();
+  }, []);
+
+  return <span>{reposLength}</span>;
+}
