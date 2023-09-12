82aef7cdd04084969f821ceac20b549fa2e555d7
francescoalterio <francescoalteriog@gmail.com>
2023-09-12 19:57:18 -0400

    add courses section

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,54 +1,64 @@
 export interface Project {
-  id: number;
-  name: string;
-  projectType: projectType;
-  fileName: string;
-  alt: string;
-  description: string;
-  preview: {
-    name: string;
-    url: string;
-  }[];
+  id: number
+  name: string
+  projectType: projectType
+  fileName: string
+  alt: string
+  description: string
+  preview: Array<{
+    name: string
+    url: string
+  }>
 
-  repository: {
-    name: string;
-    url: string;
-  }[];
-  technologies: string[];
-  isFavorite: boolean;
+  repository: Array<{
+    name: string
+    url: string
+  }>
+  technologies: string[]
+  isFavorite: boolean
 }
 
 export interface Technology {
-  name: string;
-  fileName: string;
-  documentationURL: string;
+  name: string
+  fileName: string
+  documentationURL: string
 }
 
 export interface TechnologyWithType extends Technology {
-  type: technologyType;
+  type: technologyType
 }
 
 export type projectFilter =
-  | "isFavorite"
-  | "All"
-  | "Javascript"
-  | "Typescript"
-  | "PHP"
-  | "Python";
+  | 'isFavorite'
+  | 'All'
+  | 'Javascript'
+  | 'Typescript'
+  | 'PHP'
+  | 'Python'
 
 export type projectType =
-  | "Web"
-  | "Mobile"
-  | "Fullstack"
-  | "Library"
-  | "Framework"
-  | "API"
-  | "Desktop"
-  | "Terminal";
+  | 'Web'
+  | 'Mobile'
+  | 'Fullstack'
+  | 'Library'
+  | 'Framework'
+  | 'API'
+  | 'Desktop'
+  | 'Terminal'
 
 export type technologyType =
-  | "languages"
-  | "frameworks-and-libraries"
-  | "styling-and-structuring"
-  | "testing"
-  | "databases";
+  | 'languages'
+  | 'frameworks-and-libraries'
+  | 'styling-and-structuring'
+  | 'testing'
+  | 'databases'
+
+export interface Course {
+  id: number
+  title: string
+  logo: string
+  url: string
+  certificationUrl: string
+  platform: string
+  porcentage: number
+}
